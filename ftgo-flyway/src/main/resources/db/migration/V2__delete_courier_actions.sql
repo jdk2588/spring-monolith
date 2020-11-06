10ef4f12d4fcb0d305cb50be8fc865fc1fb34a09
Jaideep K <jdk2588@gmail.com>
2020-11-06 22:17:30 +0530

    Change script for V2.

--- ftgo-flyway/src/main/resources/db/migration/V2__delete_courier_actions.sql
+++ ftgo-flyway/src/main/resources/db/migration/V2__delete_courier_actions.sql
@@ -1,3 +1,9 @@
 use ftgo;
 
-DROP TABLE courier_actions;
+CREATE TABLE people
+(
+    id         BIGINT NOT NULL,
+    first_name VARCHAR(255),
+    last_name  VARCHAR(255),
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
